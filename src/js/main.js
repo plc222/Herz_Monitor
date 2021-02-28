19938fd1bed37e048a09841283c26180386de3db
plc222 <yurij.altay220@yandex.ru>
2021-02-28 19:24:25 +0700

    Стилизовал секцию Carousel

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: '<button type="button" class="slick-prev"><img src = "../img/slider/chevron-right-solid.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src = "../img/slider/chevron-left-solid.svg"></button>'
+  });
+});
+
+
+
+  
